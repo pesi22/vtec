b01541b0d956ca90dbb3e238fb58fa9c32f11dcd
Pétur Magnús <peturpe309@nemi.tskoli.is>
2017-02-28 22:51:28 +0000

    ++

---
+++ WebStoreProduct/Controllers/ProductsController.cs
@@ -0,0 +1,88 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using WebStoreProduct.Models;
+
+namespace WebStoreProduct.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private CategoriesContext db = new CategoriesContext();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductId,ProductName,Description,Quantity,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,ProductName,Description,Quantity,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
---
+++ WebStoreProduct/Models/WebStoreModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebStoreProduct.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //I am almost positive this does not belong, I believe
+        //This is creating a one to one relationship,
+        //Really you just need your Category Model to be a Foreign Key or Constraint of the Product Model
+        public virtual List<Product> Product { get; set; }
+    }
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public virtual Category Category { get; set; }
+    }
+
+    public class CategoriesContext : DbContext
+    {
+        public DbSet<Category> Categories { get;}
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class CategoryDBInitializer : DropCreateDatabaseAlways<CategoriesContext>
+    {
+        protected override void Seed(CategoriesContext context)
+        {
+            IList<Category> defaultStandards = new List<Category>();
+
+            defaultStandards.Add(new Category() { Id = 1, Name = "Books & Audible" });
+            defaultStandards.Add(new Category() { Id = 2, Name = "Movies, Music & Games" });
+            defaultStandards.Add(new Category() { Id = 3, Name = "Electronics & Computers" });
+            defaultStandards.Add(new Category() { Id = 4, Name = "Home, Garden & Tools" });
+            defaultStandards.Add(new Category() { Id = 5, Name = "Beauty, Health & Grocery" });
+            defaultStandards.Add(new Category() { Id = 6, Name = "Toys, Kids & Baby" });
+            defaultStandards.Add(new Category() { Id = 7, Name = "Clothing, Shoes & Jewelry" });
+            defaultStandards.Add(new Category() { Id = 8, Name = "Sport & Outdoors" });
+            defaultStandards.Add(new Category() { Id = 9, Name = "Automotive & Industrial" });
+            defaultStandards.Add(new Category() { Id = 10, Name = "All Categories" });
+
+            foreach (Category std in defaultStandards)
+                context.Categories.Add(std);
+
+            base.Seed(context);
+        }
+    }
+
+}
